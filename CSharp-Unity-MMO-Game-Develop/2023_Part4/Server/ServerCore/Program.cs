d54e1c2eb989385274c7e44310f604b126034665
셩잇님 <shung_@kakao.com>
2023-09-06 02:02:12 +0900

    230904 게임 서버 멀티쓰레드 컴파일러 최적화 및 캐시 이론
    자세한 내용은 블로그 기고!

--- CSharp-Unity-MMO-Game-Develop/2023_Part4/Server/ServerCore/Program.cs
+++ CSharp-Unity-MMO-Game-Develop/2023_Part4/Server/ServerCore/Program.cs
@@ -5,54 +5,49 @@ namespace ServerCore
 {
     class Program
     {
-        static void MainThread(object state)
-        {
-            for (int i = 0; i < 5; i++)
-                Console.WriteLine("Hello, Thread!");
-        }
-
-        // 메인 직원
         static void Main(string[] args)
         {
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMaxThreads(5, 5);
+            // 캐시가 정말 잘 작동하는가? 확인을 위한 테스트
+            // 5*5라고 가정해보자
+            int[,] arr = new int[10000, 10000];
 
-            for (int i = 0; i < 5; i++)
+            // 2개의 테스트
+
+            // 순차적으로 접근, 캐시에서는 지역성 법칙 때문에 인접 주소를 접근하겠구나~ 싶어서 들고있다
+            // 캐시에 이미 있으므로 캐시 히트 상황이라 빠르게 접근할 수 있다
+            // [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] 
+
+            // 시간 측정
             {
-                Task ta = new Task(() => { while (true) { } }, TaskCreationOptions.LongRunning);
-                ta.Start();
-            }
-
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    ThreadPool.QueueUserWorkItem((obj) => { while (true) { } });
-            //}
-
-            ThreadPool.QueueUserWorkItem(MainThread);
-
-            for (int i = 0; i < 1000; i ++)
-            {
-                // 직원을 한명 더 고용하고, 일을 시킨다.
-                Thread t = new Thread(MainThread);
-                // 스레드의 이름을 설정한다
-                // t.Name = "Test Thread";
-
-                // 스레드를 백그라운드 스레드 또는 포그라운드 스레드로 만들지 결정한다.
-                // true = 백그라운드 스레드, false = 포그라운드 스레드
-                t.IsBackground = true;
-                t.Start();
+                long now = DateTime.Now.Ticks;
+                for (int i = 0; i < 10000; i++)
+                    for (int j = 0; j < 10000; j++)
+                        arr[i, j] = 1;
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(i, j 순서 걸린 시간 {end - now}");
             }
 
 
-            Console.WriteLine("Waiting for Thread");
-            // 백그라운드 스레드의 종료를 기다린다.
-            t.Join();
-
-            //Console.WriteLine("Hello, World!");
-            while (true)
+            // [] [] [] [] []
+            // [] [] [] [] []
+            // [] [] [] [] []
+            // [] [] [] [] []
+            // [] [] [] [] [] 
+            // ↑ 방향으로 접근하여 상대적으로 느리다. 따라서 공간 지역성이 떨어진다. 캐시를 활용할 수 없는 상황.
+            // → 방향이 아니라 
             {
-
+                long now = DateTime.Now.Ticks;
+                for (int i = 0; i < 10000; i++)
+                    for (int j = 0; j < 10000; j++)
+                        arr[j, i] = 1;
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(j, i 순서 걸린 시간 {end - now}");
             }
+
+
+
+            // 따라서 멀티스레드가 아닌환경에서도 캐시는 잘 작동하며,
+            // 멀티 쓰레드로 넘어갈수록 이 상황은 더욱 끔찍해진다 ^^..
         }
     }
 }

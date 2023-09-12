c9cfb7d19d3253373c719071e4dc3de043bc6723
셩잇님 <shung_@kakao.com>
2023-09-12 23:04:59 +0900

    230912 게임 서버 멀티쓰레드 Lock 기초, Deadlock 학습
    자세한 내용은 블로그 기고!

--- CSharp-Unity-MMO-Game-Develop/2023_Part4/Server/ServerCore/Program.cs
+++ CSharp-Unity-MMO-Game-Develop/2023_Part4/Server/ServerCore/Program.cs
@@ -1,22 +1,76 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using System.Threading;
 
 namespace ServerCore
 {
+    class FastLock
+    {
+        public int id;
+    }
+
+    class SessionManager
+    {
+        FastLock l;
+
+        static object _lock = new object();
+
+        public static void TestSession()
+        {
+            lock (_lock)
+            {
+
+            }
+        }
+
+        public static void Test()
+        {
+            lock (_lock)
+            {
+                UserManager.TestUser();
+            }
+        }
+    }
+
+    class UserManager
+    {
+        FastLock l;
+
+        static object _lock = new object();
+
+        public static void Test()
+        {
+            lock (_lock)
+            {
+                SessionManager.TestSession();
+            }
+        }
+
+        public static void TestUser()
+        {
+            lock( _lock)
+            {
+
+            }
+        }
+    }
+
     class Program
     {
-        static int number = 0;
-
         static void Thread_1()
         {
-            for (int i = 0; i < 100000; i++)
-                number++;
+            for (int i = 0; i < 100; i++)
+            {
+                SessionManager.Test();
+            }
         }
 
         static void Thread_2()
         {
-            for (int i = 0; i < 100000; i++)
-                number--;
+            for (int i = 0; i < 100; i++)
+            {
+                UserManager.Test();
+            }
         }
 
         static void Main(string[] args)
@@ -24,10 +78,12 @@ namespace ServerCore
             Task t1 = new Task(Thread_1);
             Task t2 = new Task(Thread_2);
             t1.Start();
+
+            Thread.Sleep(100);
+
             t2.Start();
 
             Task.WaitAll(t1, t2);
-            Console.WriteLine(number);
         }
     }
 }

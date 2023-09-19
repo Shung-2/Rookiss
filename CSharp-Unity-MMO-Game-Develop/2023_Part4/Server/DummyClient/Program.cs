9f170aa9ace6c0759c73344fc24ae0d7051d5d08
셩잇님 <shung_@kakao.com>
2023-09-19 20:42:23 +0900

    230919 게임 서버 네트워크 프로그래밍 네트워크 기초 이론, OSI 7, 소켓
    자세한 내용은 블로그 기고!

--- CSharp-Unity-MMO-Game-Develop/2023_Part4/Server/DummyClient/Program.cs
+++ CSharp-Unity-MMO-Game-Develop/2023_Part4/Server/DummyClient/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace DummyClient
 {
@@ -6,7 +9,42 @@ namespace DummyClient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            // 휴대폰 설정(=손님 소켓 생성)
+            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // 문지기에게 연결한다
+                // endPoint로 입장을 요청 문의한다.
+                socket.Connect(endPoint);
+                // 로그를 통해 연결 확인
+                Console.WriteLine($"Connected To {socket.RemoteEndPoint.ToString()}");
+
+                // 보낸다
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World!");
+                int sendBytes = socket.Send(sendBuff);
+
+                // 받는다
+                // 서버가 나한테 얼마를 보낼 지 모르므로 크게 설정한다
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = socket.Receive(recvBuff);
+                // 받은 데이터를 문자열로 변환한다.
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"[From Server] {recvData}");
+
+                // 닫는다
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
--- CSharp-Unity-MMO-Game-Develop/2023_Part4/Server/ServerCore/Program.cs
+++ CSharp-Unity-MMO-Game-Develop/2023_Part4/Server/ServerCore/Program.cs
@@ -1,41 +1,82 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Runtime.CompilerServices;
 
 namespace ServerCore
 {
     class Program
     {
-        // 아래와 같이 매핑하여 사용한다.
-        // static ThreadLocal<string> ThreadName = new ThreadLocal<string>();
-        static ThreadLocal<string> ThreadName = new ThreadLocal<string>(() => { return $"My Name is {Thread.CurrentThread.ManagedThreadId}"; });
-
-        static void WhoAmI()
-        {
-            // 고유의 영역에서 이름을 설정한다.
-            //ThreadName.Value = $"My Name is {Thread.CurrentThread.ManagedThreadId}";
-            //Thread.Sleep(1000);
-            //Console.WriteLine(ThreadName.Value);
-
-            bool repeat = ThreadName.IsValueCreated;
-            if (repeat)
-                Console.WriteLine(ThreadName.Value + " repeat ");
-            else
-                Console.WriteLine(ThreadName.Value);
-        }
-
         static void Main(string[] args)
         {
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMaxThreads(3, 3);
-            Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
+            // 내 로컬 컴퓨터의 호스트 이름을 가져온다.
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
 
-            // 날려준다.
-            ThreadName.Dispose();
+            // 구글과 같은 트래픽이 엄청 많은 곳은 IP 주소 여러 개를 사용하여 부하를 줄여준다.
+            IPAddress ipAddr = ipHost.AddressList[0];
+
+            // 식당으로 비유하자면 ipAddr은 식당의 주소이고, 7777은 식당의 문 번호이다.
+            // 식당 입장시 7777 포트를 식당/손님과 동일하게 사용해줘야 입장 가능하다.
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            // 문지기 구현
+            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            // 인자 값
+            // 1. 네트워크 주소 (DNS - Domain Name System)
+            // DNS - 도메인 주소를 IP 주소로 변경해주는 것
+            // 모든 웹사이트의 주소를 도메인 대신 IP로 외운다고 생각하면 벌써 머리가 아프다.
+            // 서버의 주소가 변경될 수 있으므로 도메인을 사용하는 것이 좋다.
+            // IP 버전 (IPv4, IPv6)
+            // 2. TCP, UDP 중 어떠한 것을 사용할 것인가?
+            // 3. TCP
+
+            try
+            {
+                // 문지기 교육
+                listenSocket.Bind(endPoint);
+
+                // 영업 시작
+                // backlog - 최대 대기수, 초과 시 접속 불가(fail)
+                listenSocket.Listen(10);
+
+
+                // 접속 할 때 까지 대기하기 위한 while
+                while (true)
+                {
+                    Console.WriteLine("Listening...");
+
+                    // 손님을 입장시킨다
+                    Socket clientSocket = listenSocket.Accept();
+
+                    // 손님의 하고 싶은 말을 듣는다.
+
+                    // 얼마나 많은 데이터를 받을 지 모르기에 큰 배열로 설정한다.
+                    byte[] recvBuff = new byte[1024];
+                    // clientSocket.Receive를 통해 recvBuff에 실제 정보가 얼마나 담겼는지 확인한다.
+                    int recvBytes = clientSocket.Receive(recvBuff);
+                    // 문자열을 이용할 것이므로 Encoding 한다.
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                    Console.WriteLine($"[From Client] {recvData}");
+
+                    // 보낸다.
+
+                    // 환영 메시지를 보낸다.
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG Server!");
+                    clientSocket.Send(sendBuff);
+
+                    // 쫓아낸다.
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }

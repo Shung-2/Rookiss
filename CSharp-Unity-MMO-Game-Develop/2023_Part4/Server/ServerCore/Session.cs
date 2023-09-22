be5fc8f02cdf0ca53ea51b14e8bde0b0900a39f2
셩잇님 <shung_@kakao.com>
2023-09-22 18:59:22 +0900

    230922 게임 서버 네트워크 프로그래밍 세션#2, 3
    자세한 내용은 블로그 기고!

--- CSharp-Unity-MMO-Game-Develop/2023_Part4/Server/ServerCore/Session.cs
+++ CSharp-Unity-MMO-Game-Develop/2023_Part4/Server/ServerCore/Session.cs
@@ -13,21 +13,34 @@ namespace ServerCore
         Socket _socket;
         int _disconnected = 0;
 
+        object _lock = new object();
+        Queue<byte[]> _sendQueue = new Queue<byte[]>();
+        List<ArraySegment<byte>> _pendinglist = new List<ArraySegment<byte>>();
+        SocketAsyncEventArgs _recvArgs = new SocketAsyncEventArgs();
+        SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs();
+
         public void Start(Socket socket)
         {
             _socket = socket;
 
-            SocketAsyncEventArgs recvArgs = new SocketAsyncEventArgs();
-            recvArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
-            // recvArgs.UserToken = this;
-            recvArgs.SetBuffer(new byte[1024], 0, 1024);
+            _recvArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
+            _recvArgs.SetBuffer(new byte[1024], 0, 1024);
 
-            RegisterRecv(recvArgs);
+            _sendArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnSendCompleted);
+
+            RegisterRecv();
         }
 
         public void Send(byte[] sendBuff)
         {
-            _socket.Send(sendBuff);
+            lock (_lock)
+            {
+                _sendQueue.Enqueue(sendBuff);
+                if (_pendinglist.Count == 0)
+                {
+                    RegisterSend();
+                }
+            }
         }
 
         public void Disconnect()
@@ -42,12 +55,61 @@ namespace ServerCore
         }
 
         #region 네트워크 통신
-        void RegisterRecv(SocketAsyncEventArgs args)
+        void RegisterSend()
         {
-            bool pending = _socket.ReceiveAsync(args);
+            while (_sendQueue.Count > 0)
+            {
+                byte[] buff = _sendQueue.Dequeue();
+                _pendinglist.Add(new ArraySegment<byte>(buff, 0, buff.Length));
+            }
+            _sendArgs.BufferList = _pendinglist;
+
+            bool pending = _socket.SendAsync(_sendArgs);
             if (pending == false)
             {
-                OnRecvCompleted(null, args);
+                OnSendCompleted(null, _sendArgs);
+            }
+        }
+
+        void OnSendCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            lock (_lock)
+            {
+                if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
+                {
+                    try
+                    {
+                        _sendArgs.BufferList = null;
+                        // 성공적으로 데이터를 모두 전송했으므로 리스트를 비워준다.
+                        _pendinglist.Clear();
+
+                        // 로그
+                        Console.WriteLine($"Transferred bytes: {_sendArgs.BytesTransferred}");
+
+                        if (_sendQueue.Count > 0)
+                        {
+                            RegisterSend();
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"OnSendCompleted Fail {e}");
+                    }
+                }
+                else
+                {
+                    Disconnect();
+                }
+            }
+        }
+
+        void RegisterRecv()
+        {
+            // 전역변수로 선언한 _recvArgs의 값을 이용한다.
+            bool pending = _socket.ReceiveAsync(_recvArgs);
+            if (pending == false)
+            {
+                OnRecvCompleted(null, _recvArgs);
             }
         }
 
@@ -59,7 +121,7 @@ namespace ServerCore
                 {
                     string recvData = Encoding.UTF8.GetString(args.Buffer, args.Offset, args.BytesTransferred);
                     Console.WriteLine($"[From Client] {recvData}");
-                    RegisterRecv(args);
+                    RegisterRecv();
                 }
                 catch (Exception e)
                 {
@@ -68,7 +130,7 @@ namespace ServerCore
             }
             else
             {
-
+                Disconnect();
             }
         }
         #endregion

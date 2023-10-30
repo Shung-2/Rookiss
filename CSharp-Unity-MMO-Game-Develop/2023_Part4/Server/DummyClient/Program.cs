f15af4ac0ee75e2e46a483be34b4cdac3913c69d
Shnug-Entereal <shung0209@gmail.com>
2023-10-30 18:35:02 +0900

    231030 게임 서버 네트워크 프로그래밍 Connector
    외장하드에서 카피해온 Rookiss_Copy 프로젝트에서 인강을 들으며 작업을 진행하고, Fork를 통해 받은 폴더에 폴더 자체를 복사해 주었다. 또한 sln 파일도 업로드 한다. Connector에 대한 자세한 내용은 블로그 기고!

--- CSharp-Unity-MMO-Game-Develop/2023_Part4/Server/DummyClient/Program.cs
+++ CSharp-Unity-MMO-Game-Develop/2023_Part4/Server/DummyClient/Program.cs
@@ -1,10 +1,45 @@
-﻿using System;
+﻿using ServerCore;
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
 namespace DummyClient
 {
+    class GameSession : Session
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            // 로그
+            Console.WriteLine($"OnConnected : {endPoint}");
+
+            // 보낸다
+            for (int i = 0; i < 5; i++)
+            {
+                byte[] sendBuff = Encoding.UTF8.GetBytes($"Hello World! {i}");
+                Send(sendBuff);
+            }
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            // 로그
+            Console.WriteLine($"OnDisconnected : {endPoint}");
+        }
+
+        public override void OnRecv(ArraySegment<byte> buffer)
+        {
+            string recvData = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);
+            Console.WriteLine($"[From Server] {recvData}");
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            // 로그
+            Console.WriteLine($"Transferred bytes: {numOfBytes}");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -14,37 +49,14 @@ namespace DummyClient
             IPAddress ipAddr = ipHost.AddressList[0];
             IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
+            Connector connector = new Connector();
+            connector.Connect(endPoint, () => { return new GameSession(); });
+
             while (true)
             {
-                // 휴대폰 설정(=손님 소켓 생성)
-                Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            
                 try
                 {
-                    // 문지기에게 연결한다
-                    // endPoint로 입장을 요청 문의한다.
-                    socket.Connect(endPoint);
-                    // 로그를 통해 연결 확인
-                    Console.WriteLine($"Connected To {socket.RemoteEndPoint.ToString()}");
 
-                    // 보낸다
-                    for (int i = 0; i < 5; i++)
-                    {
-                        byte[] sendBuff = Encoding.UTF8.GetBytes($"Hello World! {i}");
-                        int sendBytes = socket.Send(sendBuff);
-                    }
-
-                    // 받는다
-                    // 서버가 나한테 얼마를 보낼 지 모르므로 크게 설정한다
-                    byte[] recvBuff = new byte[1024];
-                    int recvBytes = socket.Receive(recvBuff);
-                    // 받은 데이터를 문자열로 변환한다.
-                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
-                    Console.WriteLine($"[From Server] {recvData}");
-
-                    // 닫는다
-                    socket.Shutdown(SocketShutdown.Both);
-                    socket.Close();
                 }
                 catch (Exception e)
                 {

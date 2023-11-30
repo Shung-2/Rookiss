4b2a048857f8da2cb4527ca60511d6a351a4745b
Shnug-Entereal <shung0209@gmail.com>
2023-11-30 18:23:10 +0900

    231130 게임 서버 네트워크 프로그래밍 Serialization #2
    블로그 기고 해야함!

--- CSharp-Unity-MMO-Game-Develop/2023_Part4/Server/DummyClient/ServerSession.cs
+++ CSharp-Unity-MMO-Game-Develop/2023_Part4/Server/DummyClient/ServerSession.cs
@@ -8,21 +8,61 @@ using System.Threading.Tasks;
 
 namespace DummyClient
 {
-    class Packet
+    public abstract class Packet
     {
         public ushort size;
         public ushort packetId;
+
+        public abstract ArraySegment<byte> Write();
+        public abstract void Read(ArraySegment<byte> s);
     }
 
     class PlayerInfoReq : Packet
     {
         public long playerId;
-    }
 
-    class PlayerInfoOk : Packet
-    {
-        public int hp;
-        public int attack;
+        // PlayerInfoReq 생성자
+        public PlayerInfoReq()
+        {
+            this.packetId = (ushort)PacketID.PlayerInfoReq;
+        }
+
+        public override void Read(ArraySegment<byte> s)
+        {
+            ushort count = 0;
+
+            ushort size = BitConverter.ToUInt16(s.Array, s.Offset);
+            count += 2;
+            ushort id = BitConverter.ToUInt16(s.Array, s.Offset + count);
+            count += 2;
+
+            this.playerId = BitConverter.ToInt64(s.Array, s.Offset + count);
+            BitConverter.ToInt64(new ReadOnlySpan<byte>(s.Array, s.Offset + count, s.Count - count));
+            count += 8;
+        }
+
+        public override ArraySegment<byte> Write()
+        {
+            ArraySegment<byte> s = SendBufferHelper.Open(4096);
+
+            ushort count = 0;
+            bool success = true;
+
+            // success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count), packet.size);
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), this.packetId);
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), this.playerId);
+            count += 8;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count), (ushort)count);
+
+            if (success)
+            {
+                return null;
+            }
+
+            return SendBufferHelper.Close(count);
+        }
     }
 
     public enum PacketID
@@ -38,29 +78,15 @@ namespace DummyClient
             // 로그
             Console.WriteLine($"OnConnected : {endPoint}");
 
-            PlayerInfoReq packet = new PlayerInfoReq() { size = 4, packetId = (ushort)PacketID.PlayerInfoReq, playerId = 1001 };
+            PlayerInfoReq packet = new PlayerInfoReq() { playerId = 1001 };
 
             // 보낸다
             //for (int i = 0; i < 5; i++)
             {
-                ArraySegment<byte> s = SendBufferHelper.Open(4096);
-
-                ushort count = 0;
-                bool success = true;
-
-                // success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count), packet.size);
-                count += 2;
-                success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), packet.packetId);
-                count += 2;
-                success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), packet.playerId);
-                count += 8;
-                success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count), (ushort)count);
-                
-                ArraySegment<byte> sendBuff = SendBufferHelper.Close(count);
-
-                if (success)
+                ArraySegment<byte> s = packet.Write();
+                if (s != null)
                 {
-                    Send(sendBuff);
+                    Send(s);
                 }
             }
         }

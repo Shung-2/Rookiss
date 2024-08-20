9f7063302132ebadb146cae32ed5e4e472b05eed
Shnug-Entereal <shung0209@gmail.com>
2024-08-20 15:24:12 +0900

    240820 고급 C# 문법, LINQ #1 수강
    자세한 내용은 블로그 기고 예정~

---
+++ CSharp-Unity-MMO-Game-Develop/2024_Part6/AdvanceSyntax/AdvanceSyntax/Async.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace AdvanceSyntax
+{
+    // async/await
+    // async 이름만 봐도.. 비동기 프로그래밍
+    // 게임서버를 수강했다면 비동기 = 멀티쓰레드? → 꼭 그렇지는 않습니다.
+    // 유니티) Coroutine = 일종의 비동기, 그러나 100% 싱글쓰레드
+
+    //class Async
+    //{
+    //    // 1. Task 기초
+    //    //static Task Test()
+    //    //{
+    //    //    Console.WriteLine("Start Test");
+    //    //    Task t = Task.Delay(3000);
+    //    //    return t;
+    //    //}
+
+    //    // 2. Async 기초
+    //    //static void TestAsync()
+    //    //{
+    //    //    Console.WriteLine("Start TestAsync");
+    //    //    Task t = Task.Delay(3000);
+    //    //    t.Wait();
+    //    //    Console.WriteLine("End TestAsync");
+    //    //}
+
+    //    // Async 기초의 문제점
+    //    //static void TestAsync2()
+    //    //{
+    //    //    Console.WriteLine("Start TestAsync");
+    //    //    Task t = Task.Delay(3000); // 복잡한 작업 (ex. DB/파일 작업)일 경우 꼼짝없이 기다려야 하므로, 시스템이 먹통이 될 수 있다.
+    //    //    t.Wait();
+    //    //    Console.WriteLine("End TestAsync");
+    //    //}
+
+    //    // 3. Async 기초 문제점 해결
+    //    //static async void TestAsync3()
+    //    //{
+    //    //    Console.WriteLine("Start TestAsync");
+    //    //    // await을 통해 주도권 전달(=비동기 처리)을 할 수있도록 처리하낟.
+    //    //    Task t = Task.Delay(3000);
+    //    //    await t;
+    //    //    // await Task.Delay(3000); 위 두 줄을 한 줄로 줄일 수 있다.
+    //    //    Console.WriteLine("End TestAsync");
+    //    //}
+
+    //    // 4. 값을 반환하는 Async
+    //    //static async Task<int> TestAsync4()
+    //    //{
+    //    //    Console.WriteLine("Start TestAsync");
+    //    //    await Task.Delay(3000);
+    //    //    Console.WriteLine("End TestAsync");
+    //    //    return 100;
+    //    //}
+
+    //    // 아이스 아메리카노를 제조중 (1분)
+    //    // 주문 대기 상태
+    //    //static async Task<int> TestAsync()
+    //    //{
+    //    //    Console.WriteLine("Start TestAsync");
+    //    //    // Task t = Task.Delay(3000); // 복잡한 작업 (ex. DB나 파일 작업)
+
+    //    //    // 1.
+    //    //    // t.Wait();
+    //    //    // 2.
+    //    //    // await t; // Main문으로 제어권을 넘겨준다. (비동기)
+    //    //    // 3. 
+    //    //    await Task.Delay(3000); // 비동기로 만들어준다. (비동기)
+
+    //    //    // 작업 스레드
+    //    //    Console.WriteLine("End TestAsync");
+    //    //    return 1;
+    //    //}
+
+    //    //static async Task Main(string[] args)
+    //    //{
+    //    //    // 1. Task 기초
+    //    //    // Task t = Test();
+    //    //    // t.Wait();
+
+    //    //    // 2. Async 기초
+    //    //    // TestAsync();
+
+    //    //    // 3. Async 기초 문제점 해결
+    //    //    // TestAsync3();
+
+    //    //    // 4. 값을 반환하는 Async
+    //    //    int ret = await TestAsync4();
+
+    //    //    Console.WriteLine("while start");
+    //    //    Console.WriteLine(ret);
+
+    //    //    // 주 스레드
+    //    //    while (true)
+    //    //    {
+
+    //    //    }
+    //    //}
+    //}
+}
---
+++ CSharp-Unity-MMO-Game-Develop/2024_Part6/AdvanceSyntax/AdvanceSyntax/LINQ.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceSyntax
+{
+    public enum ClassType
+    {
+        Knight,
+        Archer,
+        Mage
+    }
+
+    public class Player
+    {
+        public ClassType ClassType { get; set; }
+        public int Level { get; set; }
+        public int Hp { get; set; }
+        public int Attack { get; set; }
+    }
+
+    class LINQ
+    {
+        static List<Player> _players = new List<Player>();
+
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                // 랜덤으로 직업 선택
+                ClassType type = ClassType.Knight;
+                switch (rand.Next(0, 3))
+                {
+                    case 0:
+                        type = ClassType.Knight;
+                        break;
+                    case 1:
+                        type = ClassType.Archer;
+                        break;
+                    case 2:
+                        type = ClassType.Mage;
+                        break;
+                }
+
+                // 플레이어 생성
+                Player player = new Player()
+                {
+                    ClassType = type,
+                    Level = rand.Next(1, 100),
+                    Hp = rand.Next(100, 1000),
+                    Attack = rand.Next(10, 100)
+                };
+
+                _players.Add(player);
+            }
+
+            // Q. 레벨이 50 이상인 Knight만 추려내서, 레벨을 낮음 → 높은 순서로 정렬할 경우
+            
+            // 일반 버전
+            {
+                List<Player> players = GetHighLevelKnights();
+                foreach (Player p in players)
+                {
+                    // 레벨과 체력 출력
+                    Console.WriteLine($"Level: {p.Level}, HP: {p.Hp}");
+                }
+            }
+
+            Console.WriteLine("**********");
+
+            // LINQ 버전
+            {
+                // from (foreach)
+                // where (필터 역할 = 조건에 부합하는 데이터만 걸러낸다.)
+                // order by (정렬을 수행. 기본적으로 오름차순 ascending / descending)
+                // select (최종 결과를 추출 → 가공해서 추출도 가능!)
+
+                var players =
+                    from p in _players
+                    where p.ClassType == ClassType.Knight && p.Level >= 50
+                    orderby p.Level
+                    select p;
+
+                foreach (Player p in players)
+                {
+                    // 레벨과 체력 출력
+                    Console.WriteLine($"Level: {p.Level}, HP: {p.Hp}");
+                }
+            }
+        }
+
+        static public List<Player> GetHighLevelKnights()
+        {
+            List<Player> players = new List<Player>();
+
+            foreach (Player player in _players)
+            {
+                // 방법 1
+                if (player.ClassType != ClassType.Knight)
+                    continue;
+
+                if (player.Level < 50)
+                    continue;
+
+                players.Add(player);
+            }
+
+            players.Sort((lhs, rhs) => { return lhs.Level - rhs.Level; });
+
+            return _players;
+        }
+    }
+}

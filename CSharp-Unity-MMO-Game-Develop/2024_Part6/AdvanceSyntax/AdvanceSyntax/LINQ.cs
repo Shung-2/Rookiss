aba4da2e5ed3cbd02e8ee47e484ea3393880a805
Shnug-Entereal <shung0209@gmail.com>
2024-08-23 12:50:20 +0900

    240823 고급 C# 문법, LINQ #2 수강
    자세한 내용은 블로그 기고 예정~

--- CSharp-Unity-MMO-Game-Develop/2024_Part6/AdvanceSyntax/AdvanceSyntax/LINQ.cs
+++ CSharp-Unity-MMO-Game-Develop/2024_Part6/AdvanceSyntax/AdvanceSyntax/LINQ.cs
@@ -20,6 +20,7 @@ namespace AdvanceSyntax
         public int Level { get; set; }
         public int Hp { get; set; }
         public int Attack { get; set; }
+        public List<int> Items { get; set; } = new List<int>();
     }
 
     class LINQ
@@ -55,6 +56,11 @@ namespace AdvanceSyntax
                     Hp = rand.Next(100, 1000),
                     Attack = rand.Next(10, 100)
                 };
+
+                for (int j = 0; j <5; j++)
+                {
+                    player.Items.Add(rand.Next(1, 101));
+                }
 
                 _players.Add(player);
             }
@@ -92,6 +98,53 @@ namespace AdvanceSyntax
                     Console.WriteLine($"Level: {p.Level}, HP: {p.Hp}");
                 }
             }
+
+            // LINQ 버전 (중첩 from)
+            // ex) 아이템 목록을 추출 (아이템 Id < 30)
+            {
+                var playerItems = from p in _players
+                                  from item in p.Items
+                                  where item < 30
+                                  select new { p, item };
+
+                var li = playerItems.ToList();
+            }
+
+            // LINQ 버전 (Group By)
+            {
+                var playersByLevel = from p in _players
+                                     group p by p.Level into g
+                                     orderby g.Key
+                                     select new { g.Key, Players = g };
+            }
+
+            // LINQ 버전 (Join - 내부 조인, outer Join - 외부 조인)
+            {
+                List<int> levels = new List<int> { 1, 5, 10 };
+
+                var playerLevels = from p in _players
+                                   join l in levels on p.Level equals l
+                                   select p;
+            }
+
+            // LINQ 표준 연산자
+            {
+                var players =
+                    from p in _players
+                    where p.ClassType == ClassType.Knight && p.Level >= 50
+                    orderby p.Level
+                    select p;
+
+                // 위 내용과 동일하다.
+                // 다만, 위 아래에서 제공하는 함수의 기능의 차이가 있는데, 아래의 버전이 세부적으로 더 많은 기능을 사용하고 싶을 때 사용한다.
+                var players2 = _players
+                        .Where(p => p.ClassType == ClassType.Knight && p.Level >= 50)
+                        .OrderBy(p => p.Level)
+                        .Select(p => p);
+            }
+
+            // 여러 LINQ 메소드
+            https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable?view=net-8.0
         }
 
         static public List<Player> GetHighLevelKnights()

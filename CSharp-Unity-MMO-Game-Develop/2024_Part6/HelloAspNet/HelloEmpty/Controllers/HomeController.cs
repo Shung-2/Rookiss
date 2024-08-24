1038a65d28fd9cca1965f208e0c0f8e22199bafe
Shnug-Entereal <shung0209@gmail.com>
2024-08-24 21:42:06 +0900

    240824 ASP.NET Core 둘러보기, HelloMVC #! 수강
    자세한 내용은 블로그 기고 완료!

---
+++ CSharp-Unity-MMO-Game-Develop/2024_Part6/HelloAspNet/HelloEmpty/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using HelloEmpty.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloEmpty.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            HelloMessage msg = new HelloMessage()
+            {
+                Message = "Welcome to ASP.NET Core!"
+            };
+
+            ViewBag.Noti = "Input message and click submit";
+
+            return View(msg);
+        }
+    }
+}
--- CSharp-Unity-MMO-Game-Develop/2024_Part6/HelloAspNet/HelloEmpty/Program.cs
+++ CSharp-Unity-MMO-Game-Develop/2024_Part6/HelloAspNet/HelloEmpty/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// 구성이 어떻게 되어 있는가?
+// M - Model -      데이터 (원자재)
+// V - View -       UI (인테리어)
+// C - Controller - Controller (액션)
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();

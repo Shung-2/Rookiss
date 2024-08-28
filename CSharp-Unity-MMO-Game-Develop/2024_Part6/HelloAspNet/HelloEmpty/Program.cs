37aaec87250337eefa50ce81c4bb8094d2b3832e
Shnug-Entereal <shung0209@gmail.com>
2024-08-28 14:49:44 +0900

    240828 ASP.NET Core 둘러보기, Hello WebAPI수강
    자세한 내용은 블로그 기고 완료!

--- CSharp-Unity-MMO-Game-Develop/2024_Part6/HelloAspNet/HelloEmpty/Program.cs
+++ CSharp-Unity-MMO-Game-Develop/2024_Part6/HelloAspNet/HelloEmpty/Program.cs
@@ -8,11 +8,19 @@
 // V
 // MVVM (Model-View-ViewModel)
 
+// Web API
+// M
+// C
+// View ¾øÀ½!
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
+// builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -22,6 +30,9 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
+    // app.UseSwagger();
+    // app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -34,6 +45,7 @@ app.UseAuthorization();
 //app.MapControllerRoute(
 //    name: "default",
 //    pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
+// app.MapRazorPages();
+app.MapControllers();
 
 app.Run();

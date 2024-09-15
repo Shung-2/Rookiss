084d964e253c1c64f9bbc25791a00f6b15c31443
Shung-Laptop <shung0209@gmail.com>
2024-09-15 15:44:30 +0900

    240915 Blazor 입문, Form, Validation 수강
    자세한 내용은 블로그 기고 완료

--- CSharp-Unity-MMO-Game-Develop/2024_Part6/BlazorApp/BlazorStudy/Data/WeatherForecast.cs
+++ CSharp-Unity-MMO-Game-Develop/2024_Part6/BlazorApp/BlazorStudy/Data/WeatherForecast.cs
@@ -1,13 +1,20 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace BlazorStudy.Data
 {
 	public class WeatherForecast
 	{
 		public DateTime Date { get; set; }
 
+		[Required(ErrorMessage = "Need TemperatureC!")]
+		[Range(typeof(int), "-100", "100")]
 		public int TemperatureC { get; set; }
 
 		public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-		public string? Summary { get; set; }
+		[Required(ErrorMessage = "Need Summary!")]
+		[StringLength(10, MinimumLength = 2, ErrorMessage = "Summary must be between 2 and 10 characters.")]
+        public string? Summary { get; set; }
 	}
 }

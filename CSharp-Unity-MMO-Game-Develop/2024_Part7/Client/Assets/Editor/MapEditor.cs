5b60470098ec2b46a79d473f544333c1c8299cdd
Shnug-Entereal <shung0209@gmail.com>
2024-12-13 22:11:14 +0900

    241213 폴더 구조 변경
    커밋이 안되는 문제가 있어, 클라와 서버를 감싸고 있는 상위 폴더 삭제.

---
+++ CSharp-Unity-MMO-Game-Develop/2024_Part7/Client/Assets/Editor/MapEditor.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+#if UNITY_EDITOR
+using System.IO;
+using UnityEditor;
+#endif
+
+public class MapEditor
+{
+#if UNITY_EDITOR
+    
+    // % : Ctrl, # : Shift, & : Alt
+    // [MenuItem("Tools/GenerateMap %#g")]
+    // private static void HelloWorld()
+    // {
+    //     if (EditorUtility.DisplayDialog("Hello World", "Create?", "Create", "Cancel"))
+    //     {
+    //         new GameObject("Hello World");
+    //     }
+    // }
+    
+    [MenuItem("Tools/GenerateMap %#g")]
+    private static void GenerateMap()
+    {
+        GameObject[] gameObjects = Resources.LoadAll<GameObject>("Prefabs/Map");
+
+        foreach (GameObject go in gameObjects)
+        {
+            Tilemap tm =  Util.FindChild<Tilemap>(go, "Tilemap_Collision", true);
+        
+            // 파일을 만듭니다.
+            using (var writer = File.CreateText($"Assets/Resources/Map/{go.name}.txt"))
+            {
+                // MinX, MinY, MaxX, MaxY
+                writer.WriteLine(tm.cellBounds.xMin);
+                writer.WriteLine(tm.cellBounds.xMax);
+                writer.WriteLine(tm.cellBounds.yMin);
+                writer.WriteLine(tm.cellBounds.yMax);
+            
+                // 왼쪽 최상단부터 시작한다.
+                for (int y = tm.cellBounds.yMax; y >= tm.cellBounds.yMin; y--)
+                {
+                    for (int x = tm.cellBounds.xMin; x <= tm.cellBounds.xMax; x++)
+                    {
+                        TileBase tile = tm.GetTile(new Vector3Int(x, y, 0));
+                    
+                        // 타일이 있는 경우 "1" 작성
+                        if (tile != null)
+                        {
+                            writer.Write("1");
+                        }
+                        else
+                        {
+                            writer.Write("0");
+                        }
+                    }
+                
+                    writer.WriteLine();
+                }
+            }
+        }
+    }
+    
+#endif
+    
+}
